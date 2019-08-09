f46ccdf0f66025c39ec1779ac99c96380b16af34
Demis Souza Silva <demisouzasilva@gmail.com>
2019-08-09 02:16:17 -0300

    _sellerService.Update(seller);

---
+++ SalesWebMvc/Services/Exepitions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Exepitions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message): base(message)
+        {
+            
+        }
+    }
+}
