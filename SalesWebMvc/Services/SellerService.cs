7c914b89270def96354baeffd3d90a8dc20432a8
Demis Souza Silva <demisouzasilva@gmail.com>
2019-08-09 00:13:17 -0300

    Simple Create form

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -8,16 +8,22 @@ namespace SalesWebMvc.Services
 {
     public class SellerService
     {
-        private readonly SalesWebMvcContext _conetxt;
+        private readonly SalesWebMvcContext _context;
 
         public SellerService(SalesWebMvcContext context)
         {
-            _conetxt = context;
+            _context = context;
         }
 
         public List<Seller> FindAll()
         {
-            return _conetxt.Seller.ToList();
+            return _context.Seller.ToList();
+        }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
         }
     }
 }
